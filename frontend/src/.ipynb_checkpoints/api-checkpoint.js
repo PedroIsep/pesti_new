d8dea7ac5ef61c7fd1c53001cfbcd321eb2ee375
1200690 <1200690@isep.ipp.pt>
2024-05-28 19:57:31 +0100

    File for frontend deployment

--- frontend/src/.ipynb_checkpoints/api-checkpoint.js
+++ frontend/src/.ipynb_checkpoints/api-checkpoint.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-//const apiURL ="/choreo-apis/pesti/backend/rest-api-be2/v1"
+const apiURL ="/choreo-apis/newpesti/backend-qyo/v1"
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/" 
+  baseURL: "http://127.0.0.1:8000/" ? "http://127.0.0.1:8000/" : apiURL
 });
 
 api.interceptors.request.use(
